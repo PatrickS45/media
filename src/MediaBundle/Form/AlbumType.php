d3d2f8108e0f975c45a3125c1d2ed40bbb4f5875
PatrickS45 <tuko45@hotmail.fr>
2016-11-22 15:28:46 +0100

    creations des entities

--- src/MediaBundle/Form/AlbumType.php
+++ src/MediaBundle/Form/AlbumType.php
@@ -5,6 +5,7 @@ namespace MediaBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AlbumType extends AbstractType
 {
@@ -13,7 +14,26 @@ class AlbumType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titreAlbum')->add('artiste')->add('genre')->add('support')        ;
+        $builder->add('titreAlbum')
+                ->add('artiste')
+                ->add('genre', ChoiceType::class, array(
+        'choices'  => array(
+            'Hip-Hop' => true,
+            'Soul' => true,
+            'Rock' => true,
+        ),
+        // *this line is important*
+        'choices_as_values' => true,
+    ))
+        ->add('support', ChoiceType::class, array(
+            'choices'  => array(
+                'Vinyl' => true,
+                'CD' => true,
+                'Cassette' => true,
+            ),
+            // *this line is important*
+            'choices_as_values' => true,
+        )) ;
     }
     
     /**
